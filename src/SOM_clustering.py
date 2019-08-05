d10a500a62055e27ef88927b1397a281fbb0010f
SpaskeS <djordje.spasojevic1996@gmail.com>
2019-08-05 02:37:50 +0200

    sredjen je SOM algoritam

--- src/SOM_clustering.py
+++ src/SOM_clustering.py
@@ -1,14 +1,15 @@
 from minisom import MiniSom
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score
 import sys
 import pandas as pd
 import math
 import joblib
 
 def main():
-    if len(sys.argv) != 2:
-        print('python3 SOM_clustering.py genes/cells')
+    if len(sys.argv) != 3:
+        print('python3 SOM_clustering.py genes/cells batch/random')
         exit(1)
 
     if sys.argv[1] == 'genes':
@@ -23,18 +24,62 @@ def main():
 
     mdl = MiniSom(N, N, data.shape[1])
     mdl.random_weights_init(data)
-    mdl.train_random(data, 100)
-    # mdl.train_batch(data, 100)
-    joblib.dump(mdl, '../models/SOM_{}.joblib'.format(sys.argv[1]))
+    if sys.argv[2] == 'random':
+        mdl.train_random(data, 100)
+    else:
+        mdl.train_batch(data, 100)
 
     plt.pcolor(mdl.distance_map().T)
     if sys.argv[1] == 'genes':
-        plt.savefig('./train_random_genes')
-        # plt.savefig('train_batch_genes')
+        if sys.argv[2] == 'random':
+            plt.savefig('train_random_genes')
+        else:
+            plt.savefig('train_batch_genes')
     else:
-        plt.savefig('./train_random_cells')
-        # plt.savefig('train_batch_cells')
+        if sys.argv[2] == 'random':
+            plt.savefig('train_random_cells')
+        else:
+            plt.savefig('train_batch_cells')
     plt.show()
+
+    labels = []
+    for x in data:
+        winner = mdl.winner(x)
+        i = (winner[0], winner[1])
+        labels.append(i)
+
+    clusters = {}
+
+    if sys.argv[1] == 'cells':
+        for i in range(data.shape[0]):
+            cluster_num = labels[i]
+            if cluster_num not in clusters:
+                clusters[cluster_num] = []
+            clusters[cluster_num].append(i)
+    else:
+        for i in range(data.shape[0]):
+            cluster_num = labels[i]
+
+            if cluster_num not in clusters:
+                clusters[cluster_num] = []
+            clusters[cluster_num].append(genes[i])
+
+    if sys.argv[1] == 'genes':
+        output = open('../outputs/SOM_genes_{}.txt'.format(sys.argv[2]), 'w')
+        for (cluster, genes) in clusters.items():
+            output.write('Cluster: {}, Size: {} \n'.format(cluster, len(genes)))
+            for gene in genes:
+                output.write(gene + ', ')
+            output.write('\n\n\n')
+    else:
+        output = open('../outputs/SOM_cells_{}.txt'.format(sys.argv[2]), 'w')
+        for (cluster, cells) in clusters.items():
+            output.write('Cluster: {}, Size: {} \n'.format(cluster, len(cells)))
+            for cell in cells:
+                output.write(str(cell) + ', ')
+            output.write('\n\n\n')
+
+    joblib.dump(mdl, '../models/SOM_{}.joblib'.format(sys.argv[1]))
 
 if __name__ == '__main__':
     main()
--- src/agglomerative_clustering.py
+++ src/agglomerative_clustering.py
@@ -1,22 +1,26 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score
 import sys
 import joblib
 
 def main():
     if len(sys.argv) < 2:
-        print('python3 agglomerative_clustering.py genes/cells linkage_type(optional)')
+        print('python3 agglomerative_clustering.py genes/cells (optional)linkage_type')
         exit(1)
 
+    type = None
     if sys.argv[1] == 'genes':
         df = pd.read_csv('../data/filtered_data.csv')
         genes = df.iloc[:, 0].values
         data = df.iloc[:, 1:].to_numpy()
+        type='genes'
         n_clust = 9
     else:
         df = pd.read_csv('../data/transposed_data.csv')
         data = df.iloc[1:, :].to_numpy()
+        type='cells'
         n_clust = 8
 
     link = 'ward'
@@ -27,18 +31,42 @@ def main():
     mdl = AgglomerativeClustering(n_clusters=n_clust, affinity="euclidean", \
                                     linkage=link).fit(data)
 
-    joblib.dump(mdl, '../models/agglomerative_{}.joblib'.format(sys.argv[1]))
+    labels = mdl.labels_
+    score = silhouette_score(data, labels, metric='euclidean')
 
-    for i in range(data.shape[0]):
-        cluster_num = mdl.labels_[i]
+    if type == 'cells':
+        for i in range(data.shape[0]):
+            cluster_num = mdl.labels_[i]
+            if cluster_num not in clusters:
+                clusters[cluster_num] = []
+            clusters[cluster_num].append(i)
+    else:
+        for i in range(data.shape[0]):
+            cluster_num = mdl.labels_[i]
 
-        if cluster_num not in clusters:
-            clusters[cluster_num] = []
-        clusters[cluster_num].append(i)
+            if cluster_num not in clusters:
+                clusters[cluster_num] = []
+            clusters[cluster_num].append(genes[i])
 
+    print('\n' + 'Silhouette score = {}'.format(score) + '\n')
     for i in clusters:
-        print("Cluster {}: ". format(i), len(clusters[i]))
-        print()
+        print("Cluster {}: ".format(i), len(clusters[i]), "\n")
+
+    output = open('../outputs/agglomerative_{}_{}_{}.txt'.format(sys.argv[1], link, n_clust), 'w')
+    if type == 'genes':
+        for (cluster, genes) in clusters.items():
+            output.write('Cluster ID: {}, Size: {} \n'.format(cluster, len(genes)))
+            for gene in genes:
+                output.write(gene + ', ')
+            output.write('\n\n\n')
+    else:
+        for (cluster, cells) in clusters.items():
+            output.write('Cluster ID: {}, Size: {} \n'.format(cluster, len(cells)))
+            for cell in cells:
+                output.write(str(cell) + ', ')
+            output.write('\n\n\n')
+
+    joblib.dump(mdl, '../models/agglomerative_{}.joblib'.format(sys.argv[1]))
 
 if __name__ == '__main__':
     main()
